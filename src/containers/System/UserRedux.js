933831de1d0ebad7c8c32fe2b2b27e2ae95a9294
unknown <B2220-Hung@B2220-VHung.kv.kenda.com.tw>
2025-12-11 11:13:36 +0700

    add button language and add change user redux to menu system

---
+++ src/containers/System/UserRedux.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+class UserRedux extends Component {
+
+    state = {
+
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className='user-redux-container'>
+                    <div className='title'>User Redux Hoi Dan IT</div>
+                    <div className="user-redux-body" >
+                        <div>Thêm mới người dùng</div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserRedux);
